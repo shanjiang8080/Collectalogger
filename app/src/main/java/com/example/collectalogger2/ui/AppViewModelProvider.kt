908694d0b215405e54d872e3a450b645eed6173f
Shan Jiang <shanjiang2005@gmail.com>
2025-06-18 14:13:41 -0400

    Okay, much of the work has been done, but also much work remains to be done. Notably, the API seems to be working but there is no proper hookup for manual updating and such.

---
+++ app/src/main/java/com/example/collectalogger2/ui/AppViewModelProvider.kt
@@ -0,0 +1,33 @@
+package com.example.collectalogger2.ui
+
+import android.app.Application
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.collectalogger2.CollectaloggerApplication
+import com.example.collectalogger2.ui.gallery.GalleryViewModel
+import com.example.collectalogger2.ui.wishlist.WishListViewModel
+
+
+object AppViewModelProvider {
+    val Factory = viewModelFactory {
+        // Initializer for GalleryViewModel
+        initializer {
+            GalleryViewModel(
+                collectaloggerApplication().container
+            )
+        }
+
+        // Initializer for WishListViewModel
+        initializer {
+            WishListViewModel(
+                collectaloggerApplication().container
+            )
+        }
+    }
+}
+
+fun CreationExtras.collectaloggerApplication(): CollectaloggerApplication =
+    (this[AndroidViewModelFactory.APPLICATION_KEY] as CollectaloggerApplication)
